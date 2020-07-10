1281d327cb994b192f66069d2644535dad2a12a6
Glitch (polydactyl-aware-sage) <none>
2020-07-10 01:06:15 +0000

    📙❤️ Checkpoint ./server.js:15901432/133

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+/* USER CODE START */
+
+app.get("/api/whoami", (req, res) => {
+  res.json({"test": "yea"});
+});
+
+/* USER CODE END */
+
 
 
 // listen for requests :)
